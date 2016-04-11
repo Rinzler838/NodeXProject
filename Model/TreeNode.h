efa4039079e1e3061e60dfbad0d80d9000ff9bab
Rinzler838 <dvil4688@740s-pl-105.local>
2016-04-11 08:20:32 -0600

    Added TreeNode Classes

---
+++ Model/TreeNode.h
@@ -0,0 +1,34 @@
+//
+//  TreeNode.hpp
+//  NodeXProject
+//
+//  Created by Villalobos, Deigen on 4/11/16.
+//  Copyright © 2016 CTEC. All rights reserved.
+//
+
+#ifndef TreeNode_h
+#define TreeNode_h
+
+#include <stdio.h>
+#include "Node.h"
+
+template <class Type>
+class TreeNode : public Node<Type>
+{
+private:
+    TreeNode<Type> * leftChild;
+    TreeNode<Type> * rightChild;
+    TreeNode<Type> * parent;
+public:
+    TreeNode();
+    TreeNode(const Type & value);
+    TreeNode(const Type & value, TreeNode<Type> * parent);
+    void setLeftChild(TreeNode<Type> * leftChild);
+    void setRightChild(TreeNode<Type> * rightChild);
+    void setParent(TreeNode<Type> * parent);
+    TreeNode<Type> * getLtChide();
+    TreeNode<Type> * getRightChild();
+    TreeNode<Type> * getParent();
+};
+
+#endif /* TreeNode_h */
